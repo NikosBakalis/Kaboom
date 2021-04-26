2d696a0a24dfd337451832de679a6a7cda9801bd
NikosBakalis <bakalis.nikolas@gmail.com>
2021-04-26 13:21:34 +0300

    Two more sounds added.

--- app/src/main/java/com/tikola/kaboom/LiakozzzSoundBoard.java
+++ app/src/main/java/com/tikola/kaboom/LiakozzzSoundBoard.java
@@ -2,6 +2,7 @@ package com.tikola.kaboom;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
@@ -9,7 +10,7 @@ import android.widget.Button;
 
 public class LiakozzzSoundBoard extends AppCompatActivity {
 
-    Button Uuuu;
+    Button Uuuu, monosSou, kalaEsu;
 
     MediaPlayer mediaPlayer;
 
@@ -19,13 +20,24 @@ public class LiakozzzSoundBoard extends AppCompatActivity {
         setContentView(R.layout.activity_liakozzz_sound_board);
 
         Uuuu = (Button) findViewById(R.id.buttonUuuuSound);
+        monosSou = (Button) findViewById(R.id.buttonMonosSouSound);
+        kalaEsu = (Button) findViewById(R.id.buttonKalaEsuSound);
+    }
+
+    public void playSound(Context context, int soundSource){
+        mediaPlayer = MediaPlayer.create(context, soundSource);
+        mediaPlayer.start();
     }
 
     public void playUuuuSound(View view) {
-        mediaPlayer = MediaPlayer.create(LiakozzzSoundBoard.this, R.raw.test);
-        if (mediaPlayer.isPlaying()) {
-            mediaPlayer.stop();
-        }
-        mediaPlayer.start();
+        playSound(LiakozzzSoundBoard.this, R.raw.test);
+    }
+
+    public void playMonosSouSound(View view) {
+        playSound(LiakozzzSoundBoard.this, R.raw.test);
+    }
+
+    public void  playKalaEsuSound(View view) {
+        playSound(LiakozzzSoundBoard.this, R.raw.test);
     }
 }
